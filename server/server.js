aa92d6317ba92d7407bc22424aee534d6cb202e1
Paresh <pareshhub-web-github-emailpareshgangavane993@gmail.com>
2026-08-10 23:28:47 +0530

    Fix server route

--- server/server.js
+++ server/server.js
@@ -4,22 +4,23 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const jobRoutes = require("./routes/jobRoutes");
+
 console.log("jobRoutes loaded:", typeof jobRoutes);
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Test route
 app.get("/", (req, res) => {
   res.send("TEST SERVER 12345");
 });
 
+// Job routes
 app.use("/api/jobs", jobRoutes);
 
-app.get("/", (req, res) => {
-  res.send("TEST SERVER 12345");
-});
-
+// Connect MongoDB and start server
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
@@ -28,7 +29,7 @@ mongoose
     const PORT = process.env.PORT || 5000;
 
     app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((error) => {
